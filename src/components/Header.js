aaf22972758b30d12e00b16219b21ab51d648dd8
Edita-TheEksperts <edita.latifi@the-eksperts.com>
2025-07-17 14:08:48 +0200

    updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -25,7 +25,7 @@ const navLinks = [
       { name: 'Betreuung zuhause organisieren', path: '/betreuung-zuhause-organisieren' },
     ],
   },
-  { name: 'Jobs', path: '/employee-register' },
+  { name: 'Jobs', path: '/Registrierung-Jobs' },
   { name: 'Support', 
     path: '/contact',
      subLinks: [
