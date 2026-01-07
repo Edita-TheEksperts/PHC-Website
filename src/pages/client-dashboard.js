ea7de13b1a4b7763b18eb0df36004e6dc9c99bf5
Anduela Nurshaba <anduela.nurshaba@the-eksperts.com>
2026-01-07 17:22:00 +0100

    p

--- src/pages/client-dashboard.js
+++ src/pages/client-dashboard.js
@@ -722,20 +722,7 @@ if (!updatedData.careCity || updatedData.careCity.trim() === "") {
              hover:shadow-lg hover:scale-105"
             >
               {/* Icon */}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-4 w-4"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1m0-10V5m-6 6H3"
-                />
-              </svg>
+           
               Logout
             </button>
           </header>
