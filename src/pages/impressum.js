5321f927887d9ba4967572b1c1dba874099e30ca
Edita the eksperts <edita.latifi@the-eksperts.com>
2026-01-17 05:37:33 -0800

    Update impressum with new company details

--- src/pages/impressum.js
+++ src/pages/impressum.js
@@ -6,60 +6,49 @@ const Impressum = () => {
       <h1 className="text-3xl font-bold mb-8 text-center">Impressum</h1>
 
       <section className="mb-6">
-        <h2 className="text-xl font-semibold mb-2">Angaben gemäß § 5 TMG</h2>
+        <h2 className="text-xl font-semibold mb-2">Angaben</h2>
         <p>
-          Musterfirma GmbH <br />
-          Musterstraße 12 <br />
-          12345 Musterstadt
+          Prime Home Care AG <br />
+          6343 Rotkreuz
         </p>
       </section>
 
       <section className="mb-6">
         <h2 className="text-xl font-semibold mb-2">Vertreten durch</h2>
-        <p>Max Mustermann</p>
+        <p>
+          Bettina Rykart <br />
+          Silvain Kocher
+        </p>
       </section>
 
       <section className="mb-6">
         <h2 className="text-xl font-semibold mb-2">Kontakt</h2>
         <p>
-          Telefon: +49 (0)123 456789 <br />
-          E-Mail: info@musterfirma.de
+          E-Mail: info@phc.ch
         </p>
       </section>
 
       <section className="mb-6">
         <h2 className="text-xl font-semibold mb-2">Registereintrag</h2>
         <p>
-          Eintragung im Handelsregister. <br />
-          Registergericht: Amtsgericht Musterstadt <br />
-          Registernummer: HRB 12345
-        </p>
-      </section>
-
-      <section className="mb-6">
-        <h2 className="text-xl font-semibold mb-2">Umsatzsteuer-ID</h2>
-        <p>
-          Umsatzsteuer-Identifikationsnummer gemäß §27a Umsatzsteuergesetz: DE123456789
+          UID: CHE-215.205.279
         </p>
       </section>
 
       <section className="mb-6">
         <h2 className="text-xl font-semibold mb-2">Haftungsausschluss</h2>
         <p>
-          Die Inhalte unserer Seiten wurden mit größter Sorgfalt erstellt. Für die
-          Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir jedoch keine
-          Gewähr übernehmen.
+     Die Inhalte unserer Seiten wurden mit grösster Sorgfalt erstellt. 
+  Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir jedoch keine Gewähr übernehmen.
         </p>
       </section>
 
       <section>
         <h2 className="text-xl font-semibold mb-2">Urheberrecht</h2>
         <p>
-          Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten
-          unterliegen dem deutschen Urheberrecht. Vervielfältigung, Bearbeitung,
-          Verbreitung und jede Art der Verwertung außerhalb der Grenzen des
-          Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw.
-          Erstellers.
+Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. 
+Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. 
+Erstellers.
         </p>
       </section>
     </div>
